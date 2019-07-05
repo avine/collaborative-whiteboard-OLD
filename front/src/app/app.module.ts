fa9918e018f493a7956cb8637a4211c30b160e6c
Stéphane Francel <contact@avine.io>
2019-07-05 09:34:46 +0200

    Enable PWA

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { CollaborativeWhiteboardModule } from '@collaborative-whiteboard';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CanvasBasicComponent } from './components/canvas-basic/canvas-basic.component';
 import { CanvasMirrorComponent } from './components/canvas-mirror/canvas-mirror.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HomeComponent } from './components/home/home.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { WhiteboardComponent } from './components/whiteboard/whiteboard.component';
-import { CanvasBasicComponent } from './components/canvas-basic/canvas-basic.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { CanvasBasicComponent } from './components/canvas-basic/canvas-basic.com
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CollaborativeWhiteboardModule
+    CollaborativeWhiteboardModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
