38811ae2cce27341ac071dc6335e944edbc35d9f
Stéphane Francel <contact@avine.io>
2019-09-25 12:51:25 +0200

    Update jes.config.js with jest-module-name-mapper

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-
+  moduleNameMapper: {
+    '^collaborative-whiteboard': '<rootDir>//dist/collaborative-whiteboard',
+    '^collaborative-whiteboard/(.*)': '<rootDir>//dist/collaborative-whiteboard/$1',
+    '^@collaborative-whiteboard': '<rootDir>//projects/collaborative-whiteboard/src/public-api'
+  }
 };
