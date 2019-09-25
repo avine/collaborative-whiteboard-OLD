01808b03b19519eb68770c6c805d6c54c7039cd2
Stéphane Francel <contact@avine.io>
2019-09-26 00:58:43 +0200

    cw.service spec

--- front/projects/collaborative-whiteboard/src/lib/cw.service.spec.ts
+++ front/projects/collaborative-whiteboard/src/lib/cw.service.spec.ts
@@ -1,7 +1,8 @@
+import { elementAt, first, take } from 'rxjs/operators';
+
 import { TestBed } from '@angular/core/testing';
 
 import { CwService } from './cw.service';
-import { DrawEvent } from './cw.model';
 
 describe('CwService', () => {
   beforeEach(() =>
@@ -15,21 +16,94 @@ describe('CwService', () => {
     expect(service).toBeTruthy();
   });
 
+  describe('emit', () => {
+    it('should emit emit$', done => {
+      const service: CwService = TestBed.get(CwService);
+
+      // Given
+      service.emit$.pipe(first()).subscribe(emitHandler);
+
+      // When
+      service.emit({ type: 'point', data: [0, 0] } as any);
+
+      // Then
+      function emitHandler(received: any) {
+        const expected = {
+          action: 'add',
+          events: [{ type: 'point', data: [0, 0] }],
+        };
+        expect(received).toMatchObject(expected);
+        done();
+      }
+    });
+
+    it('should emit history$', done => {
+      const service: CwService = TestBed.get(CwService);
+
+      // Given
+      service.history$.pipe(elementAt(1)).subscribe(historyHandler);
+
+      // When
+      service.emit({ type: 'point', data: [0, 0] } as any);
+
+      // Then
+      function historyHandler(received: any) {
+        const expected = [{ type: 'point', data: [0, 0] }];
+        expect(received).toMatchObject(expected);
+        done();
+      }
+    });
+
+    it('should emit history$', done => {
+      const service: CwService = TestBed.get(CwService);
+      let count = 0;
+
+      // Given
+      service.history$.pipe(take(3)).subscribe(historyHandler);
+
+      // When
+      service.emit({ type: 'point', data: [0, 0] } as any);
+      service.emit({ type: 'point', data: [1, 1] } as any);
+
+      // Then
+      function historyHandler(received: any) {
+        switch (count) {
+          case 1: {
+            const expected = [{ type: 'point', data: [0, 0] }];
+            expect(received).toMatchObject(expected);
+            break;
+          }
+          case 2: {
+            const expected = [
+              { type: 'point', data: [0, 0] },
+              { type: 'point', data: [1, 1] },
+            ];
+            expect(received).toMatchObject(expected);
+            done();
+            break;
+          }
+        }
+        count++;
+      }
+    });
+  });
+
   it('should broadcast', done => {
     const service: CwService = TestBed.get(CwService);
 
     // Given
-    const subscription = service.broadcast$.subscribe(events => {
-      // Then
-      expect(events).toBeTruthy();
-      subscription.unsubscribe();
-      done();
-    });
+    service.broadcast$.pipe(first()).subscribe(broadcastHandler);
 
     // When
     service.broadcast({
       action: 'add',
-      events: [{ type: 'point', data: [0, 0] } as DrawEvent],
+      events: [{ type: 'point', data: [0, 0] } as any],
     });
+
+    // Then
+    function broadcastHandler(received: any) {
+      expect(received).toBeTruthy();
+      done();
+    }
   });
 });
