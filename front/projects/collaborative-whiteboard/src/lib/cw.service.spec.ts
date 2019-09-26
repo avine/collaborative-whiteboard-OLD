f4ae41885fead81efffa1aff083f41b9f6d21099
Stéphane Francel <contact@avine.io>
2019-09-26 14:26:00 +0200

    Improve cw.service spec

--- front/projects/collaborative-whiteboard/src/lib/cw.service.spec.ts
+++ front/projects/collaborative-whiteboard/src/lib/cw.service.spec.ts
@@ -16,94 +16,102 @@ describe('CwService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('emit', () => {
-    it('should emit emit$', done => {
+  describe('history$', () => {
+    it('should emit empty array even when no event occured', () => {
       const service: CwService = TestBed.get(CwService);
-
+      const historyHandler = jest.fn();
+      const expected = [];
       // Given
-      service.emit$.pipe(first()).subscribe(emitHandler);
-
+      service.history$.pipe(take(1)).subscribe(historyHandler);
       // When
-      service.emit({ type: 'point', data: [0, 0] } as any);
-
       // Then
-      function emitHandler(received: any) {
-        const expected = {
-          action: 'add',
-          events: [{ type: 'point', data: [0, 0] }],
-        };
-        expect(received).toMatchObject(expected);
-        done();
-      }
-    });
-
-    it('should emit history$', done => {
-      const service: CwService = TestBed.get(CwService);
-
-      // Given
-      service.history$.pipe(elementAt(1)).subscribe(historyHandler);
-
-      // When
-      service.emit({ type: 'point', data: [0, 0] } as any);
-
-      // Then
-      function historyHandler(received: any) {
-        const expected = [{ type: 'point', data: [0, 0] }];
-        expect(received).toMatchObject(expected);
-        done();
-      }
-    });
-
-    it('should emit history$', done => {
-      const service: CwService = TestBed.get(CwService);
-      let count = 0;
-
-      // Given
-      service.history$.pipe(take(3)).subscribe(historyHandler);
-
-      // When
-      service.emit({ type: 'point', data: [0, 0] } as any);
-      service.emit({ type: 'point', data: [1, 1] } as any);
-
-      // Then
-      function historyHandler(received: any) {
-        switch (count) {
-          case 1: {
-            const expected = [{ type: 'point', data: [0, 0] }];
-            expect(received).toMatchObject(expected);
-            break;
-          }
-          case 2: {
-            const expected = [
-              { type: 'point', data: [0, 0] },
-              { type: 'point', data: [1, 1] },
-            ];
-            expect(received).toMatchObject(expected);
-            done();
-            break;
-          }
-        }
-        count++;
-      }
+      expect(historyHandler).toHaveBeenCalledWith(expected);
     });
   });
 
-  it('should broadcast', done => {
-    const service: CwService = TestBed.get(CwService);
-
-    // Given
-    service.broadcast$.pipe(first()).subscribe(broadcastHandler);
-
-    // When
-    service.broadcast({
-      action: 'add',
-      events: [{ type: 'point', data: [0, 0] } as any],
+  describe('emit()', () => {
+    it('should cause emit$ to emit', () => {
+      const service: CwService = TestBed.get(CwService);
+      const emitHandler = jest.fn();
+      const event: any = { type: 'point', data: [0, 0] };
+      const expected = {
+        action: 'add',
+        events: [expect.objectContaining(event)],
+      };
+      // Given
+      service.emit$.pipe(first()).subscribe(emitHandler);
+      // When
+      service.emit(event);
+      // Then
+      expect(emitHandler).toHaveBeenCalledWith(expected);
     });
 
-    // Then
-    function broadcastHandler(received: any) {
-      expect(received).toBeTruthy();
-      done();
-    }
+    /*it('should emit history$', () => {
+      const service: CwService = TestBed.get(CwService);
+      const historyHandler = jest.fn();
+      const event: any = { type: 'point', data: [0, 0] };
+      const expected = [expect.objectContaining(event)];
+      // Given
+      service.history$.pipe(elementAt(1)).subscribe(historyHandler);
+      // When
+      service.emit(event);
+      // Then
+      expect(historyHandler).toHaveBeenCalledWith(expected);
+    });*/
+
+    it('should cause history$ to emit', () => {
+      const service: CwService = TestBed.get(CwService);
+      const historyHandler = jest.fn();
+      const events: any = [
+        { type: 'point', data: [0, 0] },
+        { type: 'point', data: [1, 1] },
+      ];
+      const expected = [
+        [expect.objectContaining(events[0])],
+        [
+          expect.objectContaining(events[0]),
+          expect.objectContaining(events[1]),
+        ],
+      ];
+      // Given
+      service.history$.pipe(take(3)).subscribe(historyHandler);
+      // When
+      service.emit(events[0]);
+      service.emit(events[1]);
+      // Then
+      expect(historyHandler).toHaveBeenNthCalledWith(2, expected[0]);
+      expect(historyHandler).toHaveBeenNthCalledWith(3, expected[1]);
+    });
+  });
+
+  describe('broadcast()', () => {
+    it('should cause broadcast$ to emit when "add" event occurs', () => {
+      const service: CwService = TestBed.get(CwService);
+      const broadcastHandler = jest.fn();
+      const event: any = { type: 'point', data: [0, 0] };
+      const transport: any = { action: 'add', events: [event] };
+      const expected = expect.objectContaining({
+        events: [expect.objectContaining(event)],
+      });
+      // Given
+      service.broadcast$.pipe(first()).subscribe(broadcastHandler);
+      // When
+      service.broadcast(transport);
+      // Then
+      expect(broadcastHandler).toHaveBeenCalledWith(expected);
+    });
+
+    it('should NOT cause broadcast$ to emit when "remove" event occurs', () => {
+      const service: CwService = TestBed.get(CwService);
+      const broadcastHandler = jest.fn();
+      const event: any = { type: 'point', data: [0, 0], options: {} };
+      const transport: any = { action: 'remove', events: [event] };
+      // Given
+      service.broadcast$.pipe(first()).subscribe(broadcastHandler);
+      // When
+      service.broadcast(transport);
+      // Then
+      expect(broadcastHandler).not.toHaveBeenCalled();
+    });
   });
 });
