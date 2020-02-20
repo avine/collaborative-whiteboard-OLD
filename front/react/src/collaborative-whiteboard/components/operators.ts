18bdc23b6d9ed36578dc7c86e6f4c7f4ca97e0eb
Stéphane Francel <contact@avine.io>
2020-02-20 14:10:52 +0100

    Improve Draggable positionning (proper 'absolute' handling)

--- front/react/src/collaborative-whiteboard/components/operators.ts
+++ front/react/src/collaborative-whiteboard/components/operators.ts
@@ -2,24 +2,21 @@
 export const getDefaultDragPosition = () => ({ x: 16, y: 56 });
 
 export const centerDomElement = (element: HTMLElement) => {
-  element.style.position = 'absolute';
-
   const { offsetWidth, offsetHeight } = element;
   const getPosition = (offset: number) =>
     `calc(50% - ${Math.round(offset / 2)}px)`;
 
-  element.style.top = getPosition(offsetHeight);
   element.style.left = getPosition(offsetWidth);
+  element.style.top = getPosition(offsetHeight);
 };
 
 export const fitParentDomElement = (element: HTMLElement) => {
   // Fit the parent element
-  element.style.position = 'absolute';
   element.style.width = '100%';
   element.style.height = '100%';
 
   // Freeze element size
-  const { width, height } = element.getBoundingClientRect();
+  const { offsetWidth: width, offsetHeight: height } = element;
   element.style.width = `${width}px`;
   element.style.height = `${height}px`;
 
